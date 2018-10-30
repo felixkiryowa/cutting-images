bb8bac2fc0781360e8bc4342d44d7b5848d7e123
Francis Kiryowa <franciskiryowa68@gmail.com>
2018-10-29 23:16:49 -0400

    Add files for first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+   <head></head>
+   <body>
+     <form action="cut.php" method="post" enctype="multipart/form-data" id="something" class="uniForm">
+        <input name="new_image" id="new_image" size="30" type="file" class="fileUpload" />
+        <button name="submit" type="submit" class="submitButton">Upload Image</button>
+     </form>
+   
+   </body>
+</html>
